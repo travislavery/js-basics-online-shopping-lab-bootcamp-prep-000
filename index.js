2c41b4f0c8954853b24667686ffd737c7fecc783
Travis Lavery <travisalavery@gmail.com>
2017-11-21 23:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     console.log('Your shopping cart is empty.')
   } else if (cart.length === 1) {
     var itemHere = cart[0]
-    console.log(`In your cart, you have ${Object.keys(itemHere)[0]} at $${itemHere[Object.keys(itemHere)[0]]a}.`)
+    console.log(`In your cart, you have ${Object.keys(itemHere)[0]} at $${itemHere[Object.keys(itemHere)[0]]}.`)
     
   } else {
     var statement = "In your cart, you have "
