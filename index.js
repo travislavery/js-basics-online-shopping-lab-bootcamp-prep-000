c5c28f7e4e79aa77bc160fc2bf6486c3fa234b99
Travis Lavery <travisalavery@gmail.com>
2017-11-22 00:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function total() {
 function removeFromCart(item) {
   for(var i in cart) {
     if (Object.keys(cart[i])[0]===item) {
-      delete cart[i];
+      cart.pop(i);
       return cart;
     }
   }
