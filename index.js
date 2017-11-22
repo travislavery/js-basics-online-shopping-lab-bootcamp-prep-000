df43ba5e93af7e0db4d48744e73af959e7b64eeb
Travis Lavery <travisalavery@gmail.com>
2017-11-22 00:47:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,8 +70,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    var finalCost = 4
-    console.log(`Your total cost is ${finalCost}, which will be charged to the card ${cardNumber}.`)
+    var finalCost = total()
+    console.log(`Your total cost is $${finalCost}, which will be charged to the card ${cardNumber}.`)
     cart = [];
   }
 }
